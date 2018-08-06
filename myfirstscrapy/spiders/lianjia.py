e3de17b63ad7941035079e478e60c4ca56d43334
diW <di.W@hotmail.com>
2018-08-07 00:51:38 +0800

    test lianjia with houses in x,

--- myfirstscrapy/spiders/lianjia.py
+++ myfirstscrapy/spiders/lianjia.py
@@ -20,11 +20,13 @@ from scrapy import settings
 from scrapy.http.request import Request
 from myfirstscrapy.items import MyfirstscrapyItem
 
+global out_dic
+out_dic = {}
 class Myfirstscrapy(scrapy.Spider):
     name = "lianjia_xm"
     allowed_domains = ["m.lianjia.com"]
     start_urls = [
-        "https://m.lianjia.com/xm/loupan/pg1"
+        "https://m.lianjia.com/xm/loupan/nht1pg1"
     ]
 
     headers = {
@@ -32,8 +34,8 @@ class Myfirstscrapy(scrapy.Spider):
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.109 Safari/537.36"
     }
 
-    for page in range(2,4):
-        url = "https://m.lianjia.com/xm/loupan/pg{0}/".format(page)
+    for page in range(2,14):
+        url = "https://m.lianjia.com/xm/loupan/nht1pg{0}/".format(page)
         start_urls.append(url)
 
     def start_requests(self):
@@ -42,17 +44,23 @@ class Myfirstscrapy(scrapy.Spider):
 
     def parse(self, response):
         # url_dic = {}
+        global out_dic
         print(response)
         district_list = ['思明', '同安', '翔安', '集美', '湖里', '海沧']
         for li_first in response.css("li.resblock-list-item"):
-            district = li_first.css("div.resblock-location-line::attr('text')").extract()[0].split('-')[0]
+            district = li_first.css("div.resblock-location-line::text").extract()[0].split('-')[0].strip()
             if(district not in district_list):
                 continue
             title = li_first.css("li::attr('data-index')").extract()[0]
             # title = li_first.css("h3.name::text").extract()[0]
             id = li_first.css("li::attr('data-ulog-exposure')").extract()[0].split('=')[-1]
-            price = li_first.css("span.price_num").extract()[0]
-            print(title)
+            price = li_first.css("span.price_num::text").extract()[0]
+            print(title + ':' +price)
+            with open(r'C:\Users\root\Desktop\house_price.txt', 'a+', encoding='utf-8') as f:
+                f.write(title)
+                f.write(',')
+                f.write(price)
+                f.write('\n')
             # yield scrapy.Request(url, callback=self.parse_dir_contents)
         # print(url_dic)
 
